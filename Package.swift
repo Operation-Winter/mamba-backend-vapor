5b7a7bf54969e3911ffdb95a14e2b3b9fe643a85
armandkamffer <akamffer@mweb.co.za>
2020-10-04 18:57:37 +0200

    Update SPM version and package URL

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
@@ -7,9 +7,8 @@ let package = Package(
        .macOS(.v10_15)
     ],
     dependencies: [
-        // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(name: "mamba-networking", url: "https://github.com/Operation-Winter/mamba-ios-networking.git",  from: "1.0.1")
+        .package(name: "mamba-networking", url: "https://github.com/Operation-Winter/mamba-networking.git",  from: "1.0.5")
     ],
     targets: [
         .target(
@@ -19,13 +18,9 @@ let package = Package(
                 .product(name: "MambaNetworking", package: "mamba-networking")
             ],
             swiftSettings: [
-                // Enable better optimizations when building in Release configuration. Despite the use of
-                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
-                // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
                 .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
             ]
         ),
-        
         .target(name: "Run", dependencies: [
             .target(name: "App")
         ]),
