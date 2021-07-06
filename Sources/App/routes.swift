42a19e3afe3500c2237b85e2a0677f1f54b478ad
Armand Kamffer <52150410+armandkamffer@users.noreply.github.com>
2021-07-06 06:03:25 +0200

    Add back api routing

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,12 +4,12 @@ import NIO
 func routes(_ app: Application) throws {
     let planningSystem = PlanningSystem(eventLoop: app.eventLoopGroup.next())
     
-    app.webSocket(["planning", "host"]) { request, webSocket in
+    app.webSocket(["api", "planning", "host"]) { request, webSocket in
         webSocket.pingInterval = .seconds(20)
         planningSystem.connect(webSocket, type: .host)
     }
     
-    app.webSocket(["planning", "join"]) { request, webSocket in
+    app.webSocket(["api", "planning", "join"]) { request, webSocket in
         webSocket.pingInterval = .seconds(20)
         planningSystem.connect(webSocket, type: .join)
     }
