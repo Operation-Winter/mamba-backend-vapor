416b74de3f8dcdf764cc53e1f0afaaeef00d1008
Armand Kamffer <kamffer1@gmail.com>
2026-08-20 22:00:45 +0200

    Switch to new async startup

---
+++ Sources/Run/Run.swift
@@ -0,0 +1,20 @@
+import App
+import Vapor
+
+@main
+struct Run {
+    static func main() async throws {
+        var env = try Environment.detect()
+        try LoggingSystem.bootstrap(from: &env)
+
+        let app = try await Application.make(env)
+        do {
+            try configure(app)
+            try await app.execute()
+            try await app.asyncShutdown()
+        } catch {
+            try? await app.asyncShutdown()
+            throw error
+        }
+    }
+}
